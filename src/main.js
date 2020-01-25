02e27adafed660f63d27492a05bac64a55d2441e
Paul <contact@p-soulhiard.fr>
2020-01-25 18:01:51 +0100

    orbitcontrols + textures

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import postit from '/src/assets/models/PROD.glb'
 
 const loader = new GLTFLoader();
@@ -23,14 +24,10 @@ let camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHe
 
 
 // Light
-let hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 )
-hemiLight.position.set(0,100,0)
-scene.add(hemiLight)
-hemiLight.intensity = 2;
-
-let light = new THREE.PointLight( 0xff0000, 1, 100 )
-light.position.set(0,100, 0)
+var light = new THREE.AmbientLight( 0x404040 );
 scene.add(light)
+
+// Settings
 
 // Resize
 window.addEventListener( 'resize', function () {
@@ -44,6 +41,12 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 renderer.outputEncoding = THREE.sRGBEncoding
 
+// Controls
+let controls = new OrbitControls(camera,renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.maxPolarAngle = Math.PI/2;
+
 // Animate
 function animate() {
   requestAnimationFrame( animate );
@@ -55,13 +58,14 @@ loader.load(
   // resource URL
   postit,
   // called when the resource is loaded
-  function ( gltf ) {
+  function (gltf) {
     gltf.animations; // Array<THREE.AnimationClip>
     gltf.scene; // THREE.Scene
     gltf.scenes; // Array<THREE.Scene>
     gltf.cameras; // Array<THREE.Camera>
     gltf.asset; // Object
-    handleSceneLoaded( gltf );
+
+    handleSceneLoaded(gltf);
   },
   // called while loading is progressing
   function ( xhr ) {
@@ -75,19 +79,27 @@ loader.load(
 
 function handleSceneLoaded(gltf){
   scene.add(gltf.scene);
-  scene.add(light);
-  let result;
   let blenderCamera = gltf.scene.children[0];
   let blenderLight = gltf.scene.children[1];
+  let materials;
+
+
 
   gltf.scene.traverse( function( object ) {
+    objects.push(object)
     if ( object.isMesh ){
-      objects.push( object )
       object.castShadow = true;
-    };
+      object.receiveShadow = true;
+      if( object.material ) {
+        object.material.side = THREE.DoubleSide;
+        if (object.material.map) object.material.map.encoding = THREE.sRGBEncoding;
+        if (object.material.emissiveMap) object.material.emissiveMap.encoding = THREE.sRGBEncoding;
+        if (object.material.map || object.material.emissiveMap) object.material.needsUpdate = true;
+      }
+    }
   })
 
-  console.log(objects[6].material.map.image)
+  console.log(objects[17])
 
   camera.position.x = blenderCamera.position.x;
   camera.position.y = blenderCamera.position.y;
@@ -98,9 +110,9 @@ function handleSceneLoaded(gltf){
   camera.far = blenderCamera.far;
   camera.near = blenderCamera.near;
 
-  // light.castShadow = true;
-  // light.position.x = blenderLight.position.x;
-  // light.position.y = blenderLight.position.y;
-  // light.position.z = blenderLight.position.z;
+  light.castShadow = true;
+  light.position.x = blenderLight.position.x;
+  light.position.y = blenderLight.position.y;
+  light.position.z = blenderLight.position.z;
 
 }
